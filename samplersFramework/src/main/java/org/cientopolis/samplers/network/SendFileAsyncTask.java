71fb82dce2799d8ffc7b0c47f41a3dc6ddb03a30
javier7ar <javier7ar@gmail.com>
2017-03-11 15:57:46 -0300

    Dirty commit

--- samplersFramework/src/main/java/org/cientopolis/samplers/network/SendFileAsyncTask.java
+++ samplersFramework/src/main/java/org/cientopolis/samplers/network/SendFileAsyncTask.java
@@ -1,9 +1,57 @@
 package org.cientopolis.samplers.network;
 
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.File;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 /**
  * Created by Xavier on 08/03/2017.
  */
 
-public class SendFileAsyncTask {
-    /*something*/
+public class SendFileAsyncTask extends AsyncTask<File,Void,Void> {
+
+    private OkHttpClient client = new OkHttpClient();
+
+    @Override
+    protected Void doInBackground(File... params) {
+
+        MediaType JSON = MediaType.parse("application/pdf; charset=utf-8");
+        String url = "http://192.168.0.107/service/upload.php";
+
+        try {
+            for (File file: params) {
+
+                RequestBody body = new MultipartBody.Builder()
+                        .setType(MultipartBody.FORM)
+                        .addFormDataPart("fileToUpload", file.getName(),
+                                RequestBody.create(MediaType.parse("text/csv"), file))
+                        .build();
+
+                Request request = new Request.Builder()
+                        .url(url)
+                        .post(body)
+                        .build();
+
+                Log.e("request", "request:" + request.toString());
+
+                Response response = client.newCall(request).execute();
+                String respuesta = response.body().string();
+
+                Log.e("SendFile", "ok:" + respuesta);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
